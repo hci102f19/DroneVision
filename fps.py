442bdf2ddd897ee3a138f58715271d45cd762f50
Kasper Helsted <helsted@vopu.dk>
2019-02-28 10:23:07 +0100

    Added better fps and write to video

--- fps.py
+++ fps.py
@@ -14,7 +14,8 @@ from time import time, sleep
 
 import cv2
 
-from libs import done
+from model.Canny import Canny
+from model.CoordinateDampner import CoordinateDampner
 
 
 class FrameBuffer(threading.Thread):
@@ -26,6 +27,7 @@ class FrameBuffer(threading.Thread):
         self.frames = self.stream.get(cv2.CAP_PROP_FRAME_COUNT)
 
         self._current_frame = None
+        self._running = False
 
         self.size = (640, 360)
 
@@ -40,9 +42,11 @@ class FrameBuffer(threading.Thread):
         else:
             self.overflow = 0
 
+            # Busy sleep would be better.
             sleep(self.sleep_timer - exec_time)
 
     def run(self):
+        self._running = True
         success, image = self.stream.read()
 
         while success:
@@ -53,11 +57,52 @@ class FrameBuffer(threading.Thread):
 
             self.sleep(time() - start)
 
+        self._running = False
         self._current_frame = None
 
-        done()
+    def get(self):
+        return self._current_frame
+
+    def running(self):
+        return self._running
 
 
 fb = FrameBuffer()
+coordinate_dampner = CoordinateDampner(2)
 
 fb.start()
+
+count = 0
+fps = 10
+
+size = (640, 360)
+out = cv2.VideoWriter('./test.mp4', cv2.VideoWriter_fourcc(*'mp4v'), float(25), size)
+
+while fb.running():
+    frame = fb.get()
+    start = time()
+
+    if frame is not None:
+        canny = Canny(frame)
+        canny.process_frame()
+
+        point = canny.get_center()
+
+        if point is not None and point.is_valid():
+            cv2.circle(frame, (point.x, point.y), 5, (0, 0, 255), -1)
+            coordinate_dampner.enqueue(point)
+
+        d_point = coordinate_dampner.point()
+        cv2.circle(frame, (d_point.x, d_point.y), 3, (255, 255, 0), -1)
+
+        out.write(frame)
+
+        count += 1
+
+    end = time()
+
+    if end - start > 1 / fps:
+        continue
+
+    sleep((1 / fps) - (end - start))
+out.release()
