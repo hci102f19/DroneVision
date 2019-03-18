61479ccedd8967368824195eed707335560cfce4
Kasper Helsted <helsted@vopu.dk>
2019-03-18 16:58:19 +0100

    Record video

--- model/buffers/StreamBuffer.py
+++ model/buffers/StreamBuffer.py
@@ -1,5 +1,8 @@
 import os
+import uuid
 from time import sleep
+
+import cv2
 
 from model.buffers import Buffer
 from model.exceptions import MissingEnvironmentVariable
@@ -7,7 +10,7 @@ from model.logging import log
 
 
 class StreamBuffer(Buffer):
-    def __init__(self, stream, x, y):
+    def __init__(self, stream, x, y, **kwargs):
         super().__init__(stream, x, y)
 
         # Thanks:
@@ -16,6 +19,12 @@ class StreamBuffer(Buffer):
         env = os.environ.get('OPENCV_FFMPEG_CAPTURE_OPTIONS', None)
         if env is None or 'protocol_whitelist;file,rtp,udp' not in env:
             raise MissingEnvironmentVariable('Missing OPENCV_FFMPEG_CAPTURE_OPTIONS')
+
+        if kwargs.get('record', False):
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+            self.out = cv2.VideoWriter(f'./{uuid.uuid4().hex}.mp4', fourcc, stream.get(cv2.CAP_PROP_FPS), (x, y))
+        else:
+            self.out = None
 
     def run(self):
         self._running = True
@@ -28,7 +37,13 @@ class StreamBuffer(Buffer):
         while success and self._running:
             self._latest_frame = self.pre_process_frame(image)
 
+            if self.out is not None:
+                self.out.write(self._latest_frame)
+
             success, image = self.stream.read()
+
+        if self.out is not None:
+            self.out.release()
 
         self._running = False
         self._latest_frame = None
