d3072a8672b7815e1895eb33287580ecdef249c2
Kasper Helsted <khelst13@student.aau.dk>
2019-03-19 15:42:48 +0100

    New hitbox

--- model/extended_geometry/hitbox/HitBox.py
+++ model/extended_geometry/hitbox/HitBox.py
@@ -1,18 +1,40 @@
+import cv2
+from shapely.geometry import Polygon
+
 from model.flight.Vector import Vector
-from model.geometry.Box import Box
 from model.geometry.Point import Point
 
 
-class HitBox(Box):
-    def __init__(self, x1, y1, x2, y2, **kwargs):
-        super().__init__(x1, y1, x2, y2, **kwargs)
+class HitBox(Polygon):
+    def __init__(self, **kwargs):
+        super().__init__(kwargs.get('geom'))
         self.force = kwargs.get('force', 100)
-        self.rotation = kwargs.get('rotation', 0)
+        self.color = kwargs.get('color', (0, 255, 0))
 
-        self.rotate(self.rotation)
+    @staticmethod
+    def round(val):
+        return int(round(val, 0))
 
     def hit(self, point: Point, vector: Vector):
         if self.intersects(point):
             vector.set_roll(self.force)
             return True
         return False
+
+    @property
+    def center(self):
+        x, y = self.centroid.coords.xy
+        return round(x[0], 2), round(y[0], 2)
+
+    def render(self, image, color=None):
+        # Center
+        cv2.circle(image, (int(self.center[0]), int(self.center[1])), 5, self.color, -1)
+
+        x_coordinates, y_coordinates = self.boundary.coords.xy
+        coordinates = [(self.round(x), self.round(y)) for x, y in zip(x_coordinates, y_coordinates)]
+
+        # Make a line between each of the nodes
+        for idx in range(len(coordinates) - 1):
+            start = coordinates[idx]
+            end = coordinates[idx + 1]
+            cv2.line(image, start, end, (0, 0, 0), 3)
--- model/extended_geometry/hitbox/RightHitBox.py
+++ model/extended_geometry/hitbox/RightHitBox.py
@@ -1,11 +1,7 @@
-import cv2
-from shapely.geometry import Polygon
-
-from model.flight.Vector import Vector
-from model.geometry.Point import Point
+from model.extended_geometry.hitbox.HitBox import HitBox
 
 
-class RightHitBox(Polygon):
+class RightHitBox(HitBox):
     def __init__(self, width, height, **kwargs):
         width_top = width - ((width / 2) * kwargs.get('width_top', 0.97))
         width_bottom = width - ((width / 2) * kwargs.get('width_bottom', 0.6))
@@ -13,35 +9,4 @@ class RightHitBox(Polygon):
 
         init_geom = [(width, height), (width, height_top), (width_top, height_top), (width_bottom, height)]
 
-        self.force = kwargs.get('force', 100)
-        self.color = kwargs.get('color', (0, 255, 0))
-
-        super().__init__(init_geom)
-
-    @staticmethod
-    def round(val):
-        return int(round(val, 0))
-
-    def hit(self, point: Point, vector: Vector):
-        if self.intersects(point):
-            vector.set_roll(self.force)
-            return True
-        return False
-
-    @property
-    def center(self):
-        x, y = self.centroid.coords.xy
-        return round(x[0], 2), round(y[0], 2)
-
-    def render(self, image, color=None):
-        # Center
-        cv2.circle(image, (int(self.center[0]), int(self.center[1])), 5, self.color, -1)
-
-        x_coordinates, y_coordinates = self.boundary.coords.xy
-        coordinates = [(self.round(x), self.round(y)) for x, y in zip(x_coordinates, y_coordinates)]
-
-        # Make a line between each of the nodes
-        for idx in range(len(coordinates) - 1):
-            start = coordinates[idx]
-            end = coordinates[idx + 1]
-            cv2.line(image, start, end, (0, 0, 0), 3)
+        super().__init__(geom=init_geom, **kwargs)
